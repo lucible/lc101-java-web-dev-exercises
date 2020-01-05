fd322a8d168e4963b9871d664c7c6676d1c34056
lucible <gobbldygook@letterboxes.org>
2020-01-05 14:06:51 -0600

    finished class 1 reading

--- src/org/launchcode/java/demos/lsn1datatypes/Message.java
+++ src/org/launchcode/java/demos/lsn1datatypes/Message.java
@@ -8,6 +8,8 @@ public class Message {
             return "¡Hola, Mundo!";
         } else if (lang.equals("fr")) {
             return "Bonjour, le monde!";
+        } else if (lang.equals("xh")) {
+            return "Molo Lizwe!";
         } else {
             return "Hello, World!";
         }
